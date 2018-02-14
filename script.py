86b2792ed5d6d713389dfec4d98acf57f0e86cb4
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-14 15:40:32 -0800

    getting a feel of basic python functions

--- script.py
+++ script.py
@@ -5,4 +5,16 @@ Spyder Editor
 This is a temporary script file.
 """
 
-print("Hello World")
+x = 1
+y = 2
+#print(x + y)
+
+#print(x)
+
+#def add_numbers(x, y):
+#    return x + y
+#print(add_numbers(1, 2))
+
+def add_numbers(x, y, z):
+    return x + y + z
+print(add_numbers(1, 2, 3))
