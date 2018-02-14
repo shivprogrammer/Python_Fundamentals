8c72c56f0a1cdc56227ed61ced99fee9e92e47ec
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-14 15:46:07 -0800

    update to add_numbers to take 2 or 3 parameters

--- script.py
+++ script.py
@@ -7,14 +7,16 @@ This is a temporary script file.
 
 x = 1
 y = 2
-#print(x + y)
 
+#print(x + y)
 #print(x)
 
-#def add_numbers(x, y):
-#    return x + y
-#print(add_numbers(1, 2))
-
-def add_numbers(x, y, z):
+def add_numbers(x, y, z=None):
+    if (z == None):
+        return x + y
+    else:
+        return x + y + z
     return x + y + z
+
+print(add_numbers(1, 2))
 print(add_numbers(1, 2, 3))
