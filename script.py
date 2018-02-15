ad2a64bf8f55f09f7d544e08c87758506a16289b
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-14 16:19:04 -0800

    hammering out python basic functionality

--- script.py
+++ script.py
@@ -1,8 +1,8 @@
 x = 1
 y = 2
 
-#print(x + y)
-#print(x)
+#print(x + y) // return 3
+#print(x) // return 1
 
 def add_numbers(x, y, z=None):
     if (z == None):
@@ -11,8 +11,8 @@ def add_numbers(x, y, z=None):
         return x + y + z
     return x + y + z
 
-#print(add_numbers(1, 2))
-#print(add_numbers(1, 2, 3))
+#print(add_numbers(1, 2)) // return 3
+#print(add_numbers(1, 2, 3)) // return 6
 
 def do_math(a, b, kind='add'):
     if (kind == 'add'):
@@ -20,24 +20,27 @@ def do_math(a, b, kind='add'):
     else:
         return a - b
 
-#print(do_math(1, 2))
+#print(do_math(1, 2)) // return 3
 
 x = (1, 'a', 2, 'b')
-#print(type(x))
+#print(type(x)) // return tuple
 
 x = [1, 'a', 2, 'b']
-#print(type(x))
+#print(type(x)) // return list
 
 x.append(3.3)
-#print(x)
+#print(x) // return [1, 'a', 2, 'b', 3.3]
 
-"""
-for item in x:
-    print(item)
-"""
+#for item in x:
+#    print(item)
     
-i = 0
-while (i != len(x) ):
-    print(x[i])
-    i += 1
-    
+#i = 0
+#while (i != len(x) ):
+#    print(x[i])
+#    i += 1
+ 
+# BOTH OF THE PREVIOUS LOOPS RETURN THE SAME RESULT
+   
+print([1, 2] + [3, 4])
+print([1] * 3)
+print(1 in [1, 2, 3])
